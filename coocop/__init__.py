6df47bb91bdf6c51034956b62570da504164cddd
PhilipMay <eniak.info@gmail.com>
2019-09-17 20:42:52 +0200

    init for module

---
+++ coocop/__init__.py
@@ -0,0 +1,4 @@
+from .coocop import Copyout
+from .coocop import CopyPairing
+
+__version__ = '0.1.dev1'
---
+++ setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="coocop",
+    version="0.1.dev1",
+    maintainer="Philip May",
+    author="Philip May",
+    author_email="pm@eniak.de",
+    description="Copyout and CopyPaiting",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/t-systems-on-site-services-gmbh/coocop",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+    install_requires=[
+        'numpy',
+    ],
+    keywords='augmentation CNN',
+    classifiers=[
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Software Development',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+    ],
+)
