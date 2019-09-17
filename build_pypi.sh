8fa482d515b3db6363732a05c444ac352e613cd2
PhilipMay <eniak.info@gmail.com>
2019-09-17 21:21:11 +0200

    buildfile

--- build_pypi.sh
+++ build_pypi.sh
@@ -1,5 +1,6 @@
 echo Please make sure to update the version number in setup.py and __init__.py
 read -p "Press enter to continue"
 rm -r dist
+rm -r coocop.egg-info
 python3 setup.py sdist bdist_wheel
 twine upload dist/*
