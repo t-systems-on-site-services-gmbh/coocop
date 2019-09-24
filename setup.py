750dda8d3fb1d69d893521488850f0acf7f890e9
PhilipMay <eniak.info@gmail.com>
2019-09-24 08:06:52 +0200

    0.2 release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="coocop",
-    version="0.1.dev2",
+    version="0.2",
     maintainer="Philip May",
     author="Philip May",
     author_email="pm@eniak.de",
